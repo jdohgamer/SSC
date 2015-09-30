ad1b100ef2d08243177be79c3abab166d28b3b8f
Tubocass <tubocass@gmail.com>
2015-09-30 14:50:53 -0500

    Changed how the camera pans left and right during game to be animation based. Apparently you can't do animation controls and Transform controls mixed together.

--- Super Sport Club/Assets/_Scripts/LeftRight.cs
+++ Super Sport Club/Assets/_Scripts/LeftRight.cs
@@ -4,22 +4,24 @@ using System.Collections;
 public class LeftRight : MonoBehaviour {
 
 	public float speed;
-	Transform tran;
+	Animator anim;
 	// Use this for initialization
 	void Start () 
 	{
-		tran = GetComponent<Transform> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		float h = Input.GetAxisRaw ("Horizontal");
-		if (h != 0) 
-		{
-			Vector3 targetDir = new Vector3 (speed * h, 0, 0);
-			targetDir  = tran.position + targetDir *Time.deltaTime; 
-			tran.position = Vector3.MoveTowards(tran.position,targetDir,0.25f);
-		}
+		anim.SetInteger("PanDir",(int) h);
+//		if (h != 0) 
+//		{
+//			Vector3 targetDir = new Vector3 (speed * h, 0, 0);
+//			targetDir  = tran.position + targetDir *Time.deltaTime; 
+//			tran.position = Vector3.MoveTowards(tran.position,targetDir,0.25f);
+//
+//		}
 	}
 }
